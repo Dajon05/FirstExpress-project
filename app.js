e6537514fa76010af76dbb1b3ee70e34dd583101
COR Students <corstudents@Matts-MacBook-Pro-4.local>
2018-11-28 10:17:27 -0500

    firstexpressproject

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("hello world")
+});
+app.get('/about/',(req, res)=>{
+    res.send("This is the about page!")
+});
+app.listen(3000)
